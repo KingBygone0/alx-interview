ea9e5303b1aa005da6f3cabe2c24799ae0702da7
KingBygone0 <kingbygone@icloud.com>
2024-05-03 08:13:11 +0000

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,1 +1,27 @@
-ii
+#!/usr/bin/python3
+"""Solves the lock boxes puzzle """
+
+
+def canUnlockAll(boxes):
+    """
+    Function to determine if all boxes can be opened.
+    Args:
+        boxes (list): List that contains all boxes with keys.
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
+    if not boxes:
+        return False
+
+    visited = set()
+    to_explore = [0]
+    while to_explore:
+        current_box = to_explore.pop()
+        visited.add(current_box)
+
+        for key in boxes[current_box]:
+            if key not in visited and key < len(boxes):
+                if key not in to_explore:
+                    to_explore.append(key)
+
+    return len(visited) == len(boxes)
